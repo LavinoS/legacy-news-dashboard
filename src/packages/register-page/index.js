4452600d774536f992e700a8ee5e52984d37d689
LavinoS <vasile.mihalache99@gmail.com>
2023-03-08 16:59:08 +0200

    LN-11: Implemented ThemeProvider in order to send dynamic styling on styled components

--- src/packages/register-page/index.js
+++ src/packages/register-page/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { LegacyDiv } from '../../components';
+
+const DEFAULT_STYLING = {
+  ALL_DEVICES: {
+    margin: '200px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  TABLET: {
+    margin: '300px',
+  },
+  MOBILE: {
+    margin: '400px',
+  },
+};
 
 const RegisterPage = (props) => {
-  console.log(props);
+  return <LegacyDiv styleProps={DEFAULT_STYLING}></LegacyDiv>;
 };
 
 export default RegisterPage;
